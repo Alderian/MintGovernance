96e53ca858a68c0aad79cc980608712c6a4599e6
Alderian <53636+Alderian@users.noreply.github.com>
2023-01-14 12:14:47 -0300

    Added configuratoin for votingDelay and Period. Added scripts and fixed tests

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat");
+const fs = require("fs");
 
 async function main() {
   // Get owner address
@@ -9,11 +10,15 @@ async function main() {
   // gets the address of the token before it is deployed
   const futureAddress = ethers.utils.getContractAddress({
     from: owner.address,
-    nonce: transactionCount + 1
+    nonce: transactionCount + 1,
   });
 
   const MyGovernor = await ethers.getContractFactory("MyGovernor");
-  const governor = await MyGovernor.deploy(futureAddress);
+  const governor = await MyGovernor.deploy(
+    futureAddress,
+    25 /* 5 minutes */,
+    300 /* 1 hour */
+  );
 
   const MyToken = await ethers.getContractFactory("MyToken");
   const token = await MyToken.deploy(governor.address);
@@ -22,6 +27,15 @@ async function main() {
     `Governor deployed to ${governor.address}`,
     `Token deployed to ${token.address}`
   );
+
+  // Put contract addresses in this file to use in other scripts
+  fs.writeFileSync(
+    "./scripts/_contracts.js",
+    `module.exports = {\n    governorAddress: "${governor.address}",\n    tokenAddress: "${token.address}"\n};`
+  );
+
+  // 2. Delegate
+  await token.delegate(owner.address);
 }
 
 main().catch((error) => {
